3d826a34b85976e3b45ed030df63c024d4137d8c
Emanuel Marquis <49501465+Eman-00@users.noreply.github.com>
2022-08-24 14:10:17 -0300

    feat: get summoner data

--- myapp/app.js
+++ myapp/app.js
@@ -2,9 +2,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const axios = require("axios").default;
+const mconfig = require("./config");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const { config } = require('process');
 
 var app = express();
 
@@ -16,5 +19,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+const getSummonerData = (platform, summonerName) => {
+    axios.get(`https://${platform}/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${mconfig.api_key}`,)
+        .then(response => {
+            res = response;
+            console.log("response: %s", response.status);
+            console.log(response.data);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
+
+app.post("/index", (req, res, next) => {
+    getSummonerData(req.body.platform, req.body.summonerName);
+});
 
 module.exports = app;
