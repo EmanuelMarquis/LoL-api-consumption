15ee66f6a92a6c33fb08ee9f49753d2c42d48e48
Emanuel Marquis <49501465+Eman-00@users.noreply.github.com>
2022-08-25 16:48:51 -0300

    feat: show summoner's data
    show summoner's data on the /summoner route.
    
    - set ejs view engine
    - refactored getSummonerData function

--- myapp/app.js
+++ myapp/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
 const axios = require("axios").default;
 const mconfig = require("./config");
 
@@ -11,6 +12,7 @@ const { config } = require('process');
 
 var app = express();
 
+app.set("view engine", "ejs");
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,20 +22,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-const getSummonerData = (platform, summonerName) => {
-    axios.get(`https://${platform}/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${mconfig.api_key}`,)
-        .then(response => {
-            res = response;
-            console.log("response: %s", response.status);
-            console.log(response.data);
-        })
+const getSummonerData = async (platform, summonerName) => 
+    await axios.get(`https://${platform}/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${mconfig.api_key}`,)
+        .then(res => res.data)
         .catch(err => {
-            console.log(err);
+            console.log(`couldn't get summoner data: ${err.response.status}`);
+            return null;
         });
-}
 
-app.post("/index", (req, res, next) => {
-    getSummonerData(req.body.platform, req.body.summonerName);
+
+app.post("/summoner", async (req, res, next) => {
+    const sdata = await getSummonerData(req.body.platform, req.body.summonerName);
+
+    sdata ? res.render("summoner", {
+        summonerName: sdata.name,
+        summonerLevel: sdata.summonerLevel
+    }) : res.send("Couldn't get summoner data");
+    next();
 });
 
 module.exports = app;
