08e443b6a8c6e86fa4d74d93c646dcc662467e1e
Emanuel Marquis <49501465+Eman-00@users.noreply.github.com>
2022-08-26 08:46:29 -0300

    feat: show champion masteries

--- myapp/app.js
+++ myapp/app.js
@@ -3,8 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-const axios = require("axios").default;
-const mconfig = require("./config");
+const summoner = require("./Summoner");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,21 +21,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-const getSummonerData = async (platform, summonerName) => 
-    await axios.get(`https://${platform}/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${mconfig.api_key}`,)
-        .then(res => res.data)
-        .catch(err => {
-            console.log(`couldn't get summoner data: ${err.response.status}`);
-            return null;
-        });
-
-
 app.post("/summoner", async (req, res, next) => {
-    const sdata = await getSummonerData(req.body.platform, req.body.summonerName);
+    const sdata = await summoner.getData(req.body.platform, req.body.summonerName);
+    const champs = await summoner.getChampMastery(req.body.platform, sdata.id);
 
     sdata ? res.render("summoner", {
         summonerName: sdata.name,
-        summonerLevel: sdata.summonerLevel
+        summonerLevel: sdata.summonerLevel,
+
+        championName_1: champs[0].championName,
+        championPoints_1: champs[0].championPoints,
+
+        championName_2: champs[1].championName,
+        championPoints_2: champs[1].championPoints,
+
+        championName_3: champs[2].championName,
+        championPoints_3: champs[2].championPoints,
+
+        championName_4: champs[3].championName,
+        championPoints_4: champs[3].championPoints,
+
+        championName_5: champs[4].championName,
+        championPoints_5: champs[4].championPoints
     }) : res.send("Couldn't get summoner data");
     next();
 });
